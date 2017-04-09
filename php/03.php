2c14e9b5fcd1d1ff41b89eda4e55dc5343939653
Big-stone <597685607@qq.com>
2017-04-09 15:32:14 +0800

    变量类型

---
+++ php/03.php
@@ -0,0 +1,31 @@
+<?php 
+    header("Content-Type:text/html;charset=utf-8");
+
+    //  变量的类型:
+        // 1：字符串
+        // 2：整型
+        // 3：浮点型
+        // 4：boolean
+        // 5：数组
+        
+        // 1.字符串
+        $hello = "你好，世界";
+        echo $hello;
+        echo "<br/>";
+
+        // 2.整型
+        $age = 17;
+        echo $age;
+        echo "<br/>";
+
+        // 3：浮点型
+        $price = 9.99;
+        echo $price;
+        echo "<br/>";
+        
+        // 4：boolean类型 1代表true 空字符串代表false
+        // $flag=true;//1
+         $flag=false;
+         echo $flag;
+
+?>
